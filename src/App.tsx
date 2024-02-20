2fab1e49e8fc0d9b02dcd217c212758b9ca839e2
Demid <demid-aykashev@mail.ru>
2024-02-21 00:00:29 +0500

    add registration form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-
+import RegistrationForm from './components/forms/RegistrationForm/RegistrationForm';
 
 function App() {
   return (
-    <>
-    </>
+    <div className="h-dvh flex justify-center items-center">
+      <RegistrationForm />
+    </div>
   );
 }
 
--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,9 +1,10 @@
-import {  forwardRef } from 'react';
-import css from './Input.module.scss';
+import { forwardRef } from "react";
+import css from "./Input.module.scss";
 
 
 const Input = forwardRef<HTMLInputElement, React.HTMLProps<HTMLInputElement>>((props, ref) => (
-  <input ref={ref} {...props} className={`py-3 px-3 w-full max-w-64  rounded-xl border border-solid border-black ${css.shadow}`} />
+  <input ref={ref} {...props}
+         className={`py-1.5 px-3 w-full max-w-64 text-base rounded-xl border border-solid border-black ${css.shadow}`} />
 ));
 
 export default Input;
