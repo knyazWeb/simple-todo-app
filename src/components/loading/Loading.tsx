ca807ded4c24af7a6fac67e293f726dee8a5b05f
Demid <demid-aykashev@mail.ru>
2024-04-28 21:07:00 +0500

    fix loaders

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -2,7 +2,7 @@ import css from "./Loading.module.scss";
 
 const Loading = () => {
   return (
-    <div className='absolute z-10 w-screen h-screen top-0 left-0 bg-opacity-35 bg-black'>
+    <div className='absolute z-10 w-full h-full top-0 left-0 bg-opacity-35 bg-black'>
       <div className="absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-28 h-28 rounded-full bg-red-400 flex items-center justify-center ">
         <div
           className={`  absolute top-1/2 left-1/2 w-28 h-28 bg-transparent border-4 border-t-transparent  border-white rounded-full ${css.anim}`}></div>
--- src/components/success/Success.tsx
+++ src/components/success/Success.tsx
@@ -3,7 +3,7 @@ import css from './Success.module.scss'
 
 const Success = () => {
   return (
-    <div className="absolute z-10 w-screen h-screen top-0 left-0 bg-opacity-35 bg-black">
+    <div className="absolute z-10 w-full h-full top-0 left-0 bg-opacity-35 bg-black">
       <div className="absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-28 h-28 rounded-full bg-red-400 flex items-center justify-center ">
         <div className={css.arrowWrapper}>
           <MdOutlineDone size={50} color="white" />
